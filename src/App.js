5eb1c4cf06999f574abaf8ae722d555bb772db32
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-11-15 19:17:29 -0600

    feat(app): add MainNav component, add style files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-import Home from "./Components/Home";
+import Home from "./Components/Home/Home";
 
 function App() {
   return (
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home Works!</div>;
+  return (
+    <div>
+      <h1>Home Works!</h1>
+    </div>
+  );
 };
 
 export default Home;
