422df00dee782719f19fff6cdd4e6d06b0f01161
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-11-15 19:28:13 -0600

    feat(app): add prettier, add settings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import "./App.css";
+import './App.css';
 
-import Home from "./Components/Home/Home";
+import Home from './Components/Home/Home';
+import MainNav from '';
 
 function App() {
   return (
